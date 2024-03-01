0147931bd1fe5188f933cdd4e980ab7c15473d40
Gourav <112751346+Gourav830@users.noreply.github.com>
2024-03-01 16:21:15 +0530

    Add files via upload

---
+++ sticky.js
@@ -0,0 +1,56 @@
+var i=0;
+var container2 = document.getElementsByClassName("container2")[0];
+var container3 = document.getElementsByClassName("container3")[0];
+var checkBox = document.getElementById("checkBox");
+var xIcon = document.getElementById("xIcon");
+xIcon.addEventListener("click",function(){
+    typeNote();
+})
+checkBox.addEventListener("click",function(){
+    createNote();
+})
+function typeNote(){
+    if(container3.style.display == "none")
+    container3.style.display = "block";
+else{
+    container3.style.display = "none";
+}
+
+}
+function createNote(){
+    var noteText = document.getElementById("noteText").value;
+    var node0 = document.createElement("div");
+    var node1 = document.createElement("h1");
+    node1.innerHTML = noteText;
+
+    node1.setAttribute("style","width:250px;height:250px;font-size:26px;padding:25px;margin-top:10px;overflow:hidden;box-shadow:0px 10px 24px 0px rgba(0,0,0,0.75)");
+    node1.style.margin = margin();
+    node1.style.transform = rotate();
+    node1.style.background = color();
+    node0.appendChild(node1);
+    container2.insertAdjacentElement("beforeend",node0);
+    node0.addEventListener("mouseenter",function(){
+        node0.style.transform = "scale(1.1)";
+    })
+    node0.addEventListener("mouseleave",function(){
+        node0.style.transform = "scale(1)";
+    })
+
+    node0.addEventListener("dblclick",function(){
+        node0.remove();
+    })
+    document.getElementById("noteText").value = '';
+    }
+function margin(){
+    var random_margin=["-5px","-1px","-2px","2px","-px","1px","2px","6px","3px","-3px","2px","5px","1px"];
+    return random_margin[Math.floor(Math.random()*random_margin.length)];
+}
+function rotate(){
+    var random_rotate =["rotate(10deg)","rotate(-12deg)","rotate(8deg)","rotate(5deg)","rotate(-14deg)","rotate(-1deg)",] 
+    return random_rotate[Math.floor(Math.random()*random_rotate.length)];
+}
+function color(){
+    var random_color = ["white","pink","yellow","lightgrey","limegreen",];
+    if(i>random_color.length-1)i = 0;
+    return random_color[i++];
+}
